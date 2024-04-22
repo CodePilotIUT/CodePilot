793c82dd4e0ee0bae2187e2f7300f99ade785f02
Alexandre Malfreyt <alexandre.malfreyt@gmail.com>
2024-04-22 18:58:14 +0200

    Redesigned Code Blocks + added an input box + random population for debug

--- scripts/ingame/code_editor.gd
+++ scripts/ingame/code_editor.gd
@@ -1,21 +1,36 @@
 extends Container
 
+var code_block_scene = preload("res://scenes/ingame/elements/code_block.tscn")
+
 func _ready():
-	var code_block_scene = preload("res://scenes/ingame/elements/code_block.tscn")
-	for i in range(10):
+	populate_randomly(15)
+
+func populate_randomly(size):
+	for i in range(size):
 		var cb = code_block_scene.instantiate()
 		$CodeEditor.add_child(cb)
 		
+		# Chose random color
 		var r = randf()
 		var g = randf()
 		var b = randf()
-		cb.get_node("ColorRect").color = Color(r,g,b)
+		cb.get_node("background").modulate = Color(r,g,b)
 		
-		set_label(cb, str(i))
+		# Put random strings
+		var line = ""
+		for j in range(randi_range(3, 10)):
+			line += str(i) + " "
+		line.erase(line.length() - 1, 1)
+		set_label(cb, line)
+		
+		# Show randomly the input box
+		cb.get_node("%Input").visible = randi_range(0, 1)
 
 func set_label(cb, label):
-	cb.get_node("ColorRect").get_node("MarginContainer").get_node("Label").text = label
+	cb.get_node("%Label").text = label
 
+func get_label(cb):
+	return cb.get_node("%Label").text
 
 func _on_button_pressed():
 	var list = get_list_string()
@@ -24,6 +39,6 @@ func _on_button_pressed():
 func get_list_string():
 	var list = []
 	for child in $CodeEditor.get_children():
-		var text = child.get_node("ColorRect").get_node("MarginContainer").get_node("Label").text
+		var text = get_label(child)
 		list.append(text)
 	return list
