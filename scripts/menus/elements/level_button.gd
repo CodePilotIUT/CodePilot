932f7b3347a6109ffce1c476af00f98275a4c0c0
Alexandre Malfreyt <alexandre.malfreyt@gmail.com>
2024-04-24 15:17:22 +0200

    Fetch levels numbers from files, and sort in order

--- scripts/menus/elements/level_button.gd
+++ scripts/menus/elements/level_button.gd
@@ -3,13 +3,20 @@ extends Control
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/ingame/ingame.tscn")
+	print("Button pressed with path: " + %Button.get_meta("level_path"))
 
-func set_level(num: int):
+func set_level_number(num: int):
 	var label = %LevelNumber
 	label.text = str(num)
 
-func get_level() -> int:
-	return %LevelNumber.text
+func get_level_number() -> int:
+	return %LevelNumber.text.to_int()
+
+func set_level_path(path: String):
+	%Button.set_meta("level_path", path)
+
+func get_level_path() -> String:
+	return %Button.get_meta("level_path")
 
 func set_star1(is_full: bool):
 	var star = %Star1
--- scripts/menus/levels_menu.gd
+++ scripts/menus/levels_menu.gd
@@ -3,31 +3,55 @@ extends Control
 var level_button_scene: PackedScene = preload("res://scenes/menus/elements/level_button.tscn")
 
 func _ready():
-	load_level_buttons()
+	create_level_buttons()
 
-func load_level_buttons():
-	var dir := DirAccess.open("res://levels")
+func load_levels():
+	"""Load all the levels from the levels directory"""
+	var levels = []
+
+	var dir = DirAccess.open("res://levels")
 	if dir:
-		print("Directory opened")
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
-		var level_number := 0
 		while file_name != "":
-			if file_name.ends_with(".json") or true:
-				level_number += 1
-				print("Creation du bouton pour le niveau %d" % level_number)
-				create_level_button(file_name, level_number)
+			if file_name.ends_with(".json"):
+				var level_number = get_level_number_from_file("res://levels/" + file_name)
+				levels.append({"file_name": file_name, "level_number": level_number})
 			file_name = dir.get_next()
+		return levels
 	else:
 		print("Failed to open the directory.")
 
-func create_level_button(file_name: String, level_number: int):
-	var level_button := level_button_scene.instantiate()
-	%LevelsList.add_child(level_button)
-	level_button.set_level(level_number)
+func create_level_buttons():
+	"""Create the level buttons from the levels list"""
+	var levels = load_levels()
 
-func get_button_text(file_name: String) -> String:
-	return file_name.replace(".json", "").capitalize()
+	# Sort the levels by level number
+	levels.sort_custom(func(x, y): return x["level_number"] < y["level_number"])
+
+	for level in levels:
+		create_level_button(level["file_name"], level["level_number"])
+
+func create_level_button(file_name: String, level_number: int):
+	var level_button = level_button_scene.instantiate()
+	%LevelsList.add_child(level_button)
+	level_button.set_level_number(level_number)
+	level_button.set_level_path("res://levels/" + file_name)
+
+func get_level_number_from_file(path: String) -> int:
+	var file = FileAccess.open(path, FileAccess.READ)
+	if file:
+		var json_text = file.get_as_text()
+		file.close()
+		var json = JSON.new()
+		var error = json.parse(json_text)
+		if error == OK:
+			var data = json.data
+			if typeof(data) == TYPE_DICTIONARY and "level_number" in data:
+				return int(data["level_number"])
+		else:
+			print("JSON parsing error: ", error)
+	return 0
 
 func populate_with_buttons(n):
 	"""Populate the list with n button (for testing purposes)"""
