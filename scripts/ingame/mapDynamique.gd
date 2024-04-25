78fa32d1c8eb3df6ece2f0244282aed041d6e753
Alexandre Malfreyt <alexandre.malfreyt@gmail.com>
2024-04-25 22:05:10 +0200

    improved road tile selection

--- scripts/ingame/mapDynamique.gd
+++ scripts/ingame/mapDynamique.gd
@@ -4,16 +4,10 @@ extends Node3D
 
 @onready var grid_map : GridMap =  $GridMap
 
+const orientations = [0, 10, 16, 22]
+
 func randomOrientation():
-	var nbr = randf()
-	if nbr <= 0.25:
-		return 0
-	elif nbr <= 0.5:
-		return 10
-	elif nbr <= 0.75:
-		return 16
-	elif nbr <= 1:
-		return 22
+	return orientations[randi_range(0, 3)]
 
 func _ready():
 	var level = GameData.get_value_or_null("level")
@@ -22,8 +16,68 @@ func _ready():
 	
 	for i in range(len(level)):
 		for j in range(len(level[0])):
+
+			# 0 = Batiment
 			if level[i][j] == 0:
-				var batiment = round(randf()*7)
+				var batiment = randi_range(0, 7)
 				grid_map.set_cell_item(Vector3i(i, 0, j), batiment, randomOrientation())
-			else:
-				grid_map.set_cell_item(Vector3i(i, 0, j), 9)
+			
+			# 1 = Route
+			elif level[i][j] == 1:
+				var road = get_road_type_and_orientation(i, j, level)
+				var road_type = road[0]
+				var road_orientation = orientations[road[1]]
+				grid_map.set_cell_item(Vector3i(i, 0, j), road_type, road_orientation)
+
+func get_adjacent_roads(x, y, level):
+	"""Get the adjacent roads of a cell in the level."""
+	var roads = {
+		'x-': (x > 0 and level[x - 1][y] == 1),
+		'x+': (x < len(level) - 1 and level[x + 1][y] == 1),
+		'y-': (y > 0 and level[x][y - 1] == 1),
+		'y+': (y < len(level[0]) - 1 and level[x][y + 1] == 1)
+	}
+	return roads
+
+func get_road_type_and_orientation(x, y, level):
+	"""Get the road type and orientation of a cell in the level."""
+	var roads = get_adjacent_roads(x, y, level)
+	
+	if roads['x-'] and roads['x+'] and roads['y-'] and roads['y+']:
+		return [9, 0]
+	
+	elif roads['x-'] and roads['x+'] and roads['y-']:
+		return [11, 2]
+	elif roads['x-'] and roads['x+'] and roads['y+']:
+		return [11, 3]
+	elif roads['x-'] and roads['y-'] and roads['y+']:
+		return [11, 1]
+	elif roads['x+'] and roads['y-'] and roads['y+']:
+		return [11, 0]
+	
+	elif roads['x-'] and roads['x+']:
+		return [10, 2]
+	elif roads['y-'] and roads['y+']:
+		return [10, 0]
+
+	elif roads['x-'] and roads['y-']:
+		return [8, 1]
+	elif roads['x-'] and roads['y+']:
+		return [8, 3]
+	elif roads['x+'] and roads['y-']:
+		return [8, 2]
+	elif roads['x+'] and roads['y+']:
+		return [8, 0]
+	
+	elif roads['x-']:
+		return [10, 2]
+	elif roads['x+']:
+		return [10, 2]
+	elif roads['y-']:
+		return [10, 0]
+	elif roads['y+']:
+		return [10, 0]
+	
+	else:
+		return [9, 0]
+	
