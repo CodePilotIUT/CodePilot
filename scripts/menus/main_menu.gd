912a2a77066737e59a69b875e4474aceffddf4c9
Alexandre Malfreyt <alexandre.malfreyt@gmail.com>
2024-04-26 01:06:11 +0200

    Ajustements de style

--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -14,3 +14,14 @@ func _on_button_settings_pressed():
 
 func _on_button_exit_pressed():
 	get_tree().quit()
+
+func _process(_delta):
+	# if the window is resized above 1500px wide, the margins should be increased
+	var width = get_viewport().size.x
+	var content_max_width = 500.0
+	if width > content_max_width:
+		$MarginContainer.add_theme_constant_override("margin_left", width/2 - content_max_width/2)
+		$MarginContainer.add_theme_constant_override("margin_right", width/2 - content_max_width/2)
+	else:
+		$MarginContainer.add_theme_constant_override("margin_left", 100)
+		$MarginContainer.add_theme_constant_override("margin_right", 100)
