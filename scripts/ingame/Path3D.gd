c76d86c79f61d92083f68af430a647c5c839ad76
Alexandre Malfreyt <alexandre.malfreyt@gmail.com>
2024-04-25 16:34:27 +0200

    pathfinding from a point to another

--- scripts/ingame/Path3D.gd
+++ scripts/ingame/Path3D.gd
@@ -1,15 +1,16 @@
 extends Path3D
 
 const move_speed = 10.0
+const path_scale = 2
 
+func _ready(): pass
 
-func _ready():
-	var curviness = 30
-	curve.add_point(Vector3(-10, 1, -10))
-	curve.add_point(Vector3(10, 1, -10))
-	curve.add_point(Vector3(10, 1, 10))
-	curve.add_point(Vector3(-10, 1, 10))
-	curve.add_point(Vector3(-10, 1, -10))
+func draw_path_from_2D(path: Array[Vector2]):
+	curve.clear_points()
+	for point in path:
+		point *= path_scale
+		point += Vector2(1, 1)
+		curve.add_point(Vector3(point.x, 0, point.y))
 
 func _process(delta: float):
 	$PathFollow3D.progress += move_speed * delta
