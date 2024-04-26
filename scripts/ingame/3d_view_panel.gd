5932a83a77fe1a656fa63bac4cc88a9e8f55e892
Alexandre Malfreyt <alexandre.malfreyt@gmail.com>
2024-04-26 02:14:43 +0200

    Le pathfinding se lance avec le bouton "Play"

--- scripts/ingame/3d_view_panel.gd
+++ scripts/ingame/3d_view_panel.gd
@@ -4,13 +4,16 @@ extends Panel
 @onready var path_3D = map_dynamique.find_child("Path3D")
 
 func _ready():
+	init()
+
+func init():
 	if GameData.is_level_data_set():
+		# init the car position
 		path_3D.tp_to(Vector2(0, 1))
 		print(path_3D.get_current_coords_in_grid())
-
-		# wait 3 seconds
-		await get_tree().create_timer(3).timeout
-
-		path_3D.go_to(Vector2(14, 13))
 	else:
 		print("Level data not set")
+
+func _on_start_button_pressed():
+	init()
+	path_3D.go_to(Vector2(14, 13))
